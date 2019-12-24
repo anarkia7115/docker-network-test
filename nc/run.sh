58542b4b738cc7bea170299b9cd84ad6431b2c77
Gao JiaXiang <gaojxcs@gmail.com>
2019-12-24 00:26:29 -0800

    add network

--- nc/run.sh
+++ nc/run.sh
@@ -1,2 +1,2 @@
-docker run -d --name listen -p3123:3123 listen
-docker run -d --name send send
+docker run -d --network net-test --name listen -p3123:3123 listen 
+docker run -d --network net-test --name send send 
