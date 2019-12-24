72c890b55f770b58043659f72b1e7295c199f8dc
Gao JiaXiang <gaojxcs@gmail.com>
2019-12-24 00:24:49 -0800

    add network

--- nc/build.sh
+++ nc/build.sh
@@ -1,2 +1,2 @@
-docker build . -f listen.Dockerfile -t listen
-docker build . -f send.Dockerfile -t send
+docker build . -f listen.Dockerfile -t listen --build-arg file_change=1
+docker build . -f send.Dockerfile -t send --build-arg file_change=1
